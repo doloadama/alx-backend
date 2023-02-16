94695e6069545e7f3dea59827a31cf6222f5b6ad
doloadama <doloadama@outlook.com>
2023-02-16 08:22:33 +0000

    parameter value

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -81,24 +81,13 @@ class Server:
         Make sure to reuse get_page in your implementation.
         """
         dataset = self.dataset()
-        total_pages = int(math.ceil(len(dataset) // page_size + 1))
-        if page > total_pages:
-            return {
-                "page_size": 0,
-                "page": page,
-                "data": [],
-                "next_page": None,
-                "prev_page": total_pages,
-                "total_pages": total_pages
-            }
-        data = self.get_page(page, page_size)
-        next_page = page + 1 if page +1 <= total_pages else None
-        prev_page = page - 1 if page > 1 else None
-        return {
-            "page_size": len(data),
+        total_pages = len(self.dataset()) // page_size + 1
+        informations = {
             "page": page,
+            "page_size": page_size if page_size <= len(data) else len(data),
+            "total_pages": total_pages,
             "data": data,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": total_pages
+            "prev_page": page - 1 if page > 1 else None,
+            "next_page": page + 1 if page + 1 <= total_pages else None
         }
+        return informations
