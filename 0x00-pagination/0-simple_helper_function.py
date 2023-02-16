750a9427c8026bb8340dbfa0c41f08da1c11f022
doloadama <doloadama@outlook.com>
2023-02-16 07:11:37 +0000

    0. Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+0. Simple helper function
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """_summary_
+
+    Args:
+        page (_type_): _description_
+        page_size (_type_): _description_
+
+    Returns:
+        Tuple[int, int]: _description_
+    """
+    debut, fin = 0, 0
+    for i in range(page):
+        fin = debut
+        fin += page_size
+    return (debut, fin)
