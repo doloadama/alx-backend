d9ea37b09b33b279fa59c6ce4fe130247ec04562
doloadama <doloadama@outlook.com>
2023-02-21 16:51:09 +0000

    big fixed

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -27,23 +27,23 @@ class MRUCache(BaseCaching):
         <BaseCaching.Max_ITEMS>
         DISCARD the last item
         """
-        if not key or not item:
-            return
-
-        self.cache_data[key] = item
-        self.mru[key] = item
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discarded = next(iter(self.mru))
-            del self.cache_data[discarded]
-            print("DISCARD:", discarded)
-
-        if len(self.mru) > BaseCaching.MAX_ITEMS:
-            self.mru.popitem(last=False)
-        self.mru.move_to_end(key, False)
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.usage[-1]))
+                del self.cache_data[self.usage[-1]]
+                del self.usage[-1]
+            if key in self.usage:
+                del self.usage[self.usage.index(key)]
+            self.usage.append(key)
+            self.cache_data[key] = item 
 
     def get(self, key):
         """Retrieves an item by key"""
-        if key is None and key not in self.cache_data.keys():
+        if key is not None and key in self.cache_data.keys(): 
+            del self.usage[self.usage.index(key)]
+            self.usage.append(key)
+            return self.cache_data[key]
             return None
-        return self.cache_data[key]
