30f0c1c5b5d10d8d1be9fd9031230bd2b0872617
doloadama <doloadama@outlook.com>
2023-02-21 16:57:32 +0000

    code errors output fixed

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,47 +1,43 @@
 #!/usr/bin/env python3
-"""
-3. LRU Caching
+""" BaseCaching module
 """
 from base_caching import BaseCaching
-from collections import OrderedDict
 
 
 class LRUCache(BaseCaching):
     """
-    class caching system
+    FIFOCache defines a FIFO caching system
     """
-
     def __init__(self):
         """
-        call the parent init
+        Initialize the class with the parent's init method
         """
         super().__init__()
-        self.lru = OrderedDict()
+        self.usage = []
 
     def put(self, key, item):
         """
-        assign to the dictionary <self.cache_data> the <item> value
-        for the key <key>
-        if <key> or  <item> is None, this method should not do anything
-        if the number of items in <self.cache_data> is higher than that
-        <BaseCaching.Max_ITEMS>
-        DISCARD the last item
+        Cache a key-value pair
         """
-        if key and item:
-            self.lru[key] = item
-            self.lru.move_to_end(key)
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.usage[0]))
+                del self.cache_data[self.usage[0]]
+                del self.usage[0]
+            if key in self.usage:
+                del self.usage[self.usage.index(key)]
+            self.usage.append(key)
             self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discarded = next(iter(self.lru))
-            del self.cache_data[discarded]
-            print("DISCARD:", discarded)
-        if len(self.lru) > BaseCaching.MAX_ITEMS:
-            self.lru.popitem(last=False)
 
     def get(self, key):
         """
-
+        Return the value linked to a given key, or None
         """
-        if key and key in self.cache_data.keys():
+        if key is not None and key in self.cache_data.keys():
+            del self.usage[self.usage.index(key)]
+            self.usage.append(key)
             return self.cache_data[key]
         return None
