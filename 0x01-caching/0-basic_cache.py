17e9b216882dc50adb09c6db081c5173ef50f59d
doloadama <doloadama@outlook.com>
2023-02-21 09:49:00 +0000

    if statement coreected

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -34,7 +34,7 @@ class BasicCache(BaseCaching):
         if <key> is None or if the <key> doesn't exist in <sel.cache_data>
         return None
         """
-        if key is None and key in self.cache_data.keys():
+        if key is None and key not in self.cache_data.keys():
             return None
         else:
             return self.cache_data[key]
