a487eb5baf3bad89a8746e9f4288e72f71247f5f
doloadama <doloadama@outlook.com>
2023-02-21 16:52:19 +0000

    .

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,49 +1,51 @@
 #!/usr/bin/env python3
 """
-4. MRU Caching
+MRU Caching
 """
-from base_caching import BaseCaching
+
+
 from collections import OrderedDict
+
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
     """
-    class caching system
+    class MRUCache that inherits
+    from BaseCaching and is a caching system
     """
-
     def __init__(self):
-        """
-        Initialize the parent init method
-        """
         super().__init__()
-        self.mru = OrderedDict()
+        self.mru_order = OrderedDict()
 
     def put(self, key, item):
         """
-        assign to the dictionary <self.cache_data> the <item> value
-        for the key <key>
-        if <key> or  <item> is None, this method should not do anything
-        if the number of items in <self.cache_data> is higher than that
-        <BaseCaching.Max_ITEMS>
-        DISCARD the last item
+        Must assign to the dictionary
+        self.cache_data the item value for the key key
         """
-        if key is None or item is None:
-            pass
-        else:
-            length = len(self.cache_data)
-            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.usage[-1]))
-                del self.cache_data[self.usage[-1]]
-                del self.usage[-1]
-            if key in self.usage:
-                del self.usage[self.usage.index(key)]
-            self.usage.append(key)
-            self.cache_data[key] = item 
+        if not key or not item:
+            return
+
+        self.cache_data[key] = item
+        self.mru_order[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            item_discarded = next(iter(self.mru_order))
+            del self.cache_data[item_discarded]
+            print("DISCARD:", item_discarded)
+
+        if len(self.mru_order) > BaseCaching.MAX_ITEMS:
+            self.mru_order.popitem(last=False)
+
+        self.mru_order.move_to_end(key, False)
 
     def get(self, key):
-        """Retrieves an item by key"""
-        if key is not None and key in self.cache_data.keys(): 
-            del self.usage[self.usage.index(key)]
-            self.usage.append(key)
+        """
+        Must return the value in
+        self.cache_data linked to key.
+        """
+        if key in self.cache_data:
+            self.mru_order.move_to_end(key, False)
             return self.cache_data[key]
-            return None
+        return None
