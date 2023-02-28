8822af0d89d6aae93788116f84c6a1db87e27af9
doloadama <doloadama@outlook.com>
2023-02-28 17:58:18 +0000

    error handling

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -31,6 +31,8 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+babel.init_app(app, locale_selector=get_locale)
+
 app.route("/",strict_slashes=False)
 def index():
     """Hello World !!!
