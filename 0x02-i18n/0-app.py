64d3693450ca679097482d23c7a48ec4bac62270
doloadama <doloadama@outlook.com>
2023-02-28 16:31:16 +0000

    index names fixed

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,18 +5,9 @@ from flask_babel import Babel
 
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def index():
     """
     HANDLES / ROUTE
     """
-    return render_template("index.html")
-
-class Config:
-    """
-    has a language class attribute
-    """
-    LANGUAGES = ["en", "fr"]
-
-babel = Babel(app, locals=Config.LANGUAGES, timezone_selector="UTC")
-app.config.from_object(Config)
+    return render_template("0-index.html")
