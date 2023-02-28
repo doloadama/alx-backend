5b4d8c428cbc18a3174fd2cfeb39197150c1a57a
doloadama <doloadama@outlook.com>
2023-02-28 23:28:37 +0000

    added

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -52,7 +52,6 @@ def before_request():
     g.user = get_user(request.args.get("login_as"))
 
 
-@babel.localeselector
 def get_locale():
     """
     get_locale
@@ -69,6 +68,9 @@ def get_locale():
     if headers:
         return headers
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
 
 
 @babel.timezoneselector
