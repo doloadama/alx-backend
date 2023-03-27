a0fa0d5a399c4a93221bd9fac6e09d55be7a79c9
doloadama <doloadama@outlook.com>
2023-03-27 18:02:55 +0000

    syntax error

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,7 +1,7 @@
 import redis from 'redis';
 const client = redis.createClient(6379, '127.0.0.1');
 client.on('connect', () => {
-    console.log('Redis client connected to the server')
+    console.log('Redis client connected to the server');
 });
 
 client.on('error', err => console.log(`Redis client not connected to the server: ${Error}`));
