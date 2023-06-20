1fb162b26bd3e98945b5197699e28fc342a047ac
doloadama <doloadama@outlook.com>
2023-06-20 10:26:40 +0000

    async await usage

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,31 @@
+import redis from 'redis';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Handle successful connection
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Handle connection errors
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+    try {
+        const value = await getAsync(schoolName);
+        console.log(`Value for key ${schoolName}: ${value}`);
+    } catch (error) {
+        console.log(`Error retrieving value for key ${schoolName}: ${schoolName}: ${error.message}`);
+    };
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', 100);
+displaySchoolValue('HolbertonSanFrancisco');
